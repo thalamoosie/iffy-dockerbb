c2189ee67378dac014de4b27f0e1b60d61fbe2fd
thalamoosie <thalamoose@protonmail.com>
2023-08-28 20:23:03 -0400

    Added placeholder config files, working on nodebb containers

--- mongo/mongo-init.js
+++ mongo/mongo-init.js
@@ -2,14 +2,11 @@ console.log(
   "Begin MongoDB Setup Process - >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>"
 );
 
-const nodebbUser = process.env["NODEBB_USERNAME"];
-const nodebbPassword = process.env["NODEBB_PASSWORD"];
-
 db = db.getSiblingDB("nodebb");
 
 db.createUser({
-  user: nodebbUser,
-  pwd: nodebbPassword,
+  user: "nodebb",
+  pwd: "Testing1234!",
   roles: [
     { role: "readWrite", db: "nodebb" },
     { role: "clusterMonitor", db: "admin" },
